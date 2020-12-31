fdb3a35515b23a0d5310f41cdc7a9c32d6d0022c
matheus_cascalho <cascalhom@gmail.com>
2020-12-31 20:39:05 -0300

    print do texto e calculo da frequencia de cada termo (tf)

--- models/document.py
+++ models/document.py
@@ -15,12 +15,26 @@ class Document:
         """
         Print file text
         """
-        pass
+        text = ""
+        with open(self.filepath, 'r') as file:
+            for line in file:
+                text += line
+        print(text)
+
 
 
 def term_frequency(filepath: str) -> Dict[str, int]:
-    file = open(filepath, 'r')
-    lines = []
-    for line in file:
-        lines.append(line)
-
+    with open(filepath, 'r') as file:
+        lines = []
+        for line in file:
+            lines.append(line)
+    words = " ".join(lines)
+    words = words.split()
+    words = [word.lower().strip(".,!?") for word in words]
+    tf = {}
+    for word in words:
+        if word not in tf:
+            tf[word] = 1
+        else:
+            tf[word] += 1
+    return tf
--- tests/test_document.py
+++ tests/test_document.py
@@ -1,9 +1,32 @@
 import unittest
-
+import unittest.mock
+from models.document import Document, term_frequency
+import io
+import sys
+import pytest
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_term_frequency(self):
+        expected = {
+            "ola": 1,
+            "meu": 1,
+            "nome": 1,
+            "é": 2,
+            "matheus": 1,
+            "qual": 1,
+            "o": 1,
+            "seu": 1
+        }
+        tf = term_frequency('../files/d1.txt')
+        self.assertDictEqual(expected, tf)
+
+    def test_text(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        expected = "Ola, meu nome é matheus\nQual é o seu?\n"
+        Document('../files/d1.txt').text()
+        text = capturedOutput.getvalue()
+        self.assertEqual(expected, text)
 
 
 if __name__ == '__main__':
