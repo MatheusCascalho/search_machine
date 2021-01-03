258bd3754636c36894e62da262e9386e6ffc882a
matheus_cascalho <cascalhom@gmail.com>
2021-01-02 22:08:47 -0300

    cobertura de testes em 100% para classe document

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,6 +1,6 @@
 import unittest
 import unittest.mock
-from models.document import Document, term_frequency, get_name
+from models.document import Information, Query, term_frequency, get_name
 import io
 import sys
 import pytest
@@ -26,7 +26,38 @@ class MyTestCase(unittest.TestCase):
         name = get_name('../test_files/d1.txt')
         self.assertEqual(expected, name)
 
+    def test_content_property(self):
+        info = Information(name="info_teste")
+        expected = ""
+        real = info.content
+        self.assertEqual(real, expected)
 
+    def test_repr_property(self):
+        info = Information(name="info_teste")
+        expected = "info_teste"
+        real = repr(info)
+        self.assertEqual(real, expected)
+
+    def test_str_property(self):
+        info = Information(name="info_teste")
+        expected = "info_teste"
+        real = str(info)
+        self.assertEqual(real, expected)
+
+    def test_eq_property(self):
+        info1 = Information(name="info_teste")
+        info2 = Information(name="info_teste")
+        self.assertEqual(info1, info2)
+
+    def test_less_property(self):
+        info1 = Information(name="info_teste")
+        info2 = Information(name="info_teste2")
+        self.assertLess(info1, info2)
+
+    def test_query_name(self):
+        query = Query("teste")
+        expected = "QUERY"
+        self.assertEqual(expected, query.name)
 
 if __name__ == '__main__':
     unittest.main()
